cb30822abf1bdb8108e409d097289260a7f7fbd6
ebarrero <ebarrero@minsait.com>
2022-02-09 18:04:34 +0100

    - Estructuración inicial del proyecto
    - Se añaden librerías necesarias
    - Se añade icono moviesApp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
