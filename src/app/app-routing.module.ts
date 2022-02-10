9e16a092355000181a0266961ddb11f86eb3e128
ebarrero <ebarrero@minsait.com>
2022-02-10 17:42:51 +0100

    - Añadir nuevos componentes
    - Configuración de rutas en las diferentes pestañas
    - Añadir card básica para el catálogo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
-const routes: Routes = [];
+
+const ROUTES: Routes = [
+  {path: 'home', component: HomeComponent },
+  {path: 'movies', component: MoviesComponent },
+  {path: 'about', component: AboutComponent },
+  {path: '**', pathMatch: 'full', redirectTo: 'home' }
+
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(ROUTES)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    MoviesComponent
+    MoviesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
